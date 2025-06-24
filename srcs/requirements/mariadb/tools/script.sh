033b6be2977f1ef2b67cb8caa6e0155130aba6af
Yasser Boutslighoi <yboutsli@e2r8p7.1337.ma>
2025-06-24 19:48:13 +0100

    update

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -13,7 +13,6 @@ mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};"
 mariadb -uroot -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${USER_PASSWORD}';"
 mariadb -uroot -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';"
 mariadb -uroot  -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
-sleep 10
 mariadb -uroot -p${MARIADB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
 echo -e "${GREEN}Stopping MariaDB service...${NC}"
